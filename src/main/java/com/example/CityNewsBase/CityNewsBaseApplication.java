6de75b904d4af68cc4736ac938726d9a9ee4e40e
TomaszPiszczek <115466543+TomaszPiszczek@users.noreply.github.com>
2024-11-04 13:48:25 +0100

    Initial commit

---
+++ src/main/java/com/example/CityNewsBase/CityNewsBaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.CityNewsBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityNewsBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityNewsBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CityNewsBase/CityNewsBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CityNewsBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityNewsBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
