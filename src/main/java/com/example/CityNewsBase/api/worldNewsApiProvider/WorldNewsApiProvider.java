0f06649104988b7a2f206b4bbcc63ef2ee4f3b1a
Tomasz <tomasz.piszczek@microsoft.wsei.edu.pl>
2025-01-10 21:50:18 +0100

    fix

--- src/main/java/com/example/CityNewsBase/api/worldNewsApiProvider/WorldNewsApiProvider.java
+++ src/main/java/com/example/CityNewsBase/api/worldNewsApiProvider/WorldNewsApiProvider.java
@@ -1,6 +1,5 @@
 package com.example.CityNewsBase.api.worldNewsApiProvider;
 
-import com.example.CityNewsBase.config.SecretsManagerService;
 import com.example.CityNewsBase.model.worldNewsModel.WorldNews;
 import com.example.CityNewsBase.model.worldNewsModel.WorldNewsResponse;
 import lombok.RequiredArgsConstructor;
@@ -22,7 +21,8 @@ public class WorldNewsApiProvider {
     }
 
 
-   private final String API_KEY = SecretsManagerService.getSecretValue("APIKEY");
+    @Value("${newsapi.api-key}")
+    private String newsApiKey;
 
 
     public List<WorldNews> getNews(String earliestPublishDate, int offset) {
@@ -31,7 +31,7 @@ public class WorldNewsApiProvider {
         String language = "en";
         String sourceCountry = "US";
         String url = String.format("/search-news?api-key=%s&language=%s&source-country=%s&earliest-publish-date=%s&offset=%d",
-                API_KEY, language, sourceCountry, earliestPublishDate, offset);
+                newsApiKey, language, sourceCountry, earliestPublishDate, offset);
 
         WorldNewsResponse response = worldNewsWebClient.get()
                 .uri(url)
